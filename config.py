ed4128cd2614f6fa805c405bf9f2992f339f4453
hidarfaqeeh <hidarfaqeeh@gmail.com>
2023-10-07 23:30:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
       BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
       API_ID = int(os.environ.get("APP_ID", 12345))
       API_HASH = os.environ.get("API_HASH")
-      CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
+      CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "bottom")
       ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "odaygholy")
       ADMIN_ID = int(os.environ.get("ADMIN_ID", 485527614 )) 
       DB_URL = os.environ.get("DATABASE_URL", "")
